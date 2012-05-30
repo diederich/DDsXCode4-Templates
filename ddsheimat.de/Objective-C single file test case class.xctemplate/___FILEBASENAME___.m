cfff3331f05e9c8b4fb85ca820283ec38dedc9f9
Stephan Diederich <stephan.diederich@googlemail.com>
2012-05-30 18:56:37 +0200

    add single file OCUnit test case

---
+++ ddsheimat.de/Objective-C single file test case class.xctemplate/___FILEBASENAME___.m
@@ -0,0 +1,34 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ___FILEBASENAMEASIDENTIFIER___ : SenTestCase {
+
+}
+@end
+
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+- (void)setUp {
+  [super setUp];
+  
+  // Set-up code here.
+}
+
+- (void)tearDown {
+  // Tear-down code here.
+  
+  [super tearDown];
+}
+
+-(void)test___FILEBASENAMEASIDENTIFIER___ {
+  STAssertFalse(0, @"Not implemented");
+}
+
+@end
