081a60abd34df74838704fc758d46ecf1adb3d37
Stephan Diederich <stephan.diederich@gmail.com>
2013-02-18 16:53:47 +0100

    add KiWi Spec template

---
+++ ddsheimat.de/KiWi Spec.xctemplate/___FILEBASENAME___.m
@@ -0,0 +1,22 @@
+//
+//  ___FILEBASENAMEASIDENTIFIER___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#import "Kiwi.h"
+#import "<#ClassUnderTest#>.h"
+
+SPEC_BEGIN(___FILEBASENAMEASIDENTIFIER___)
+
+describe(@"<#ClassUnderTest#>", ^{
+
+  context(@"<#State this thing is in#>", ^{
+
+  });
+});
+
+SPEC_END
